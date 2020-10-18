23fcbd384aa67e0cb887048f1a42b199737c8570
HP <ds49574.ds@gmail.com>
2020-10-18 15:36:46 +0530

    Initialise DS

---
+++ src/bean/Department.java
@@ -0,0 +1,33 @@
+package bean;
+
+public class Department {
+    private String deptId;
+    private String deptName;
+    private String headName;
+
+    public String getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(String deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getHeadName() {
+        return headName;
+    }
+
+    public void setHeadName(String headName) {
+        this.headName = headName;
+    }
+
+
+}
